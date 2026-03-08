aa22b8e5f4f67fcb0cf1297b3ffebaba73bf2fe3
vt231_kso <vt231_kso@student.ztu.edu.ua>
2026-03-08 19:37:01 +0200

    Comment row

--- app/api/articles/[id]/route.ts
+++ app/api/articles/[id]/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 // import { Pool } from "pg";
